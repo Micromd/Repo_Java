a143ac9b5b2b4c5cd13469bf8a549b5ef79d7f03
micromd <micromd32@gmail.com>
2020-09-16 19:40:37 -0400

    Homework

---
+++ src/Class2/Homework.java
@@ -0,0 +1,62 @@
+package Class2;
+
+public class Homework {
+    public static void main (String[] args){
+        byte hoursInADay = 24;
+        System.out.println("There are " + hoursInADay + " hours in a day.");
+        short leapYearNumberOfDays = 366;
+        System.out.println("Maximum number of days in a year is " + leapYearNumberOfDays + "." );
+        int totalNumberOfEmployees = 53576;
+        System.out.println("Company X has " + totalNumberOfEmployees + " employees." );
+        long chinaPopulation = 1430000000L ;
+        System.out.println("China has the world’s largest population - " + chinaPopulation + " people." );
+        float mortgageRate = 3.09F ;
+        System.out.println("Mortgage rates fell to new a record low this week - " + mortgageRate + " %.");
+        double accountBalance = 99999999.99 ;
+        System.out.println("I wish i had " + accountBalance + " $ on my bank account.");
+        boolean sunRisesFromTheEast = true;
+        boolean sunRisesFromTheWest = false;
+        System.out.println("Does sun rises from the west? " + sunRisesFromTheWest + ".");
+        char firstName = 'A';
+        char lastName = 'D';
+        System.out.println( "My initials are " + firstName + lastName + ".");
+        String fullName = "Aleksandr Dokuchaev";
+        System.out.println("My full name is " + fullName + ".");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/First/Example.java
@@ -0,0 +1,42 @@
+package First;
+
+import java.util.concurrent.Flow;
+
+public class Example {
+    public static void main(String[] args){ //psvm
+        System.out.println("Hello");    //Sout
+        System.out.println("my first steps");
+        byte test = 100;
+        System.out.println(test);
+        int numberOfDays = 365;
+        System.out.println(numberOfDays);
+        /**
+         * Comments Comments Comments
+         * Blah Blah Blah
+         */
+        System.out.println("number of days in regular year is " + numberOfDays );
+        float gNumber = 9.8f;
+        System.out.println("G number on Earth is " + gNumber );
+        double numberPi = 3.14159265359;
+        System.out.println("Number Pi value is " + numberPi );
+        char firstLetter = 'A';
+        System.out.println("First letter in the Alphabet is " + firstLetter );
+        boolean examPassed = false;
+        System.out.println("Test score less than 70. Exam passed? " + examPassed);
+        examPassed = true ;
+        System.out.println("Test score more than 70. Exam passed? " + examPassed);
+
+
+
+
+    }
+    
+
+
+
+
+
+
+
+
+    }
