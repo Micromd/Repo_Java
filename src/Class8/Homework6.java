2726d570d462dc8756f712474058a545e74d61c3
micromd <micromd32@gmail.com>
2020-10-07 21:19:17 -0400

    Homework6

---
+++ src/Class8/Homework6.java
@@ -0,0 +1,67 @@
+package Class8;
+
+public class Homework6 {
+
+    /**
+     * Create a method to find average of an int-array
+     * */
+
+    public static double arrayAvg(double[] arr ){
+        double sum = 0;
+        for (int i = 0; i<arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        double result = sum/arr.length;
+        return result;
+    }
+
+
+    /**
+     *
+     * String[] names = {"john" , "michael" , "gphilipppre" , "philip" , "ilena" , "palena" , "fahry"}
+     * String nameToSearch = "philip";
+     * Create a method (NO return) that will print the index of given name in the given array
+     *
+     */
+
+    public static void nameSearch (String[] names) {
+
+        for (int i=0; i<names.length; i++){
+            if (names[i].equals("philip")){
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+    //methods check
+
+    public static void main (String[] args) {
+        double[] check = {3.5,7.3,3.5,5,23,46};
+        System.out.println(arrayAvg(check));
+        String[] names = {"john" , "michael" , "gphilipppre" , "philip" , "ilena" , "palena" , "fahry"};
+        nameSearch(names);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/First/Loop.java
+++ src/First/Loop.java
@@ -1,11 +1,28 @@
 package First;
 
 public class Loop {
+    public static int addTwoInts(int num1, int num2) {
+        int result = num1 +num2;
+        return result;
+
+
+    }
+
+    public static double divideNums(double num1, double num2) {
+        double result = 0;
+        if (num2!=0){
+            result = num1/num2;
+        }
+        return result;
+    }
+
     public static void main (String[] args){
        int[] num = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1} ;
        for (int num1:num){
            System.out.println(num);
        }
+
+       int res = addTwoInts(5,15);
 
 
 
